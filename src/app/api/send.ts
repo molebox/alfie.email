73ef4f703ab8265b954849e1cc2926b23ea682ff
molebox <hello@richardhaines.dev>
2023-05-19 22:45:54 +0200

    removed eslint

--- src/app/api/send.ts
+++ src/app/api/send.ts
@@ -12,7 +12,6 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       from: "hello@alfie.email",
       to: ["to@example.com"],
       subject: "hello world",
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       react: EmailTemplate({ firstName: "John", product: "Resend" }),
     });
 
